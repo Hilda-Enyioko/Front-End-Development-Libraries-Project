cb1761c5a7273c7a72a3562e3e13e52bf7e51171
Hilda Enyioko <enyiokohilda@gmail.com>
2025-01-10 22:50:04 +0100

    all project tests passed

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # Front-End-Development-Libraries-Project
-This is contains all projects for the FreeCodeCamp Front-End Development Libraries course
+This is contains all projects for the [FreeCodeCamp Front-End Development Libraries course](https://www.freecodecamp.org/learn/front-end-development-libraries/)
---
+++ random-quote-app/src/App.jsx
@@ -0,0 +1,135 @@
+import React from 'react'
+
+const rapQuotes = [
+  {
+    quote: 'Climb the ladder to success, escalator style',
+    author: 'Notorious B.I.G.'
+  },
+  {
+    quote: 'The best part of me is I am not you',
+    author: 'Eminem'
+  },
+  {
+    quote: 'Better late than never, but never late is better',
+    author: 'Drake'
+  },
+  {
+    quote: 'Worship Christ with the best of your portions.',
+    author: 'Kanye West'
+  },
+  {
+    quote: "I don't got to talk, the Lord defends me.",
+    author: 'Nicki Minaj'
+  },
+  {
+    quote: "You could love me, or hate me, I swear it won't make me or break me.",
+    author: 'Lil Wayne'
+  },{
+    quote: "Everytime I look up, I see God's faithfulness.",
+    author: 'Kanye West'
+  },
+  {
+    quote: "I'm the best in school, the best in the game. Excuse me but there's nobody in my late.",
+    author: 'Nicki Minaj'
+  },
+  {
+    quote: 'To be the best, you have to smell like the best, dress like the best, act like the best.',
+    author: 'Lil Wayne'
+  },
+  {
+    quote: "'Cause through the drama I can always depend on my mama",
+    author: 'Tupac'
+  },
+  {
+    quote: "It's easy to forget who you are.",
+    author: 'Kendrick Lamar'
+  },
+  {
+    quote: "The worst part of success is, to me, adapting to it. It's scary.",
+    author: 'Kendrick Lamar'
+  },
+  {
+    quote: "They have money for war but can't feed the poor.",
+    author: 'Tupac'
+  }
+]
+
+function getRandomQuote () {
+  const randomIndex = Math.floor(Math.random() * rapQuotes.length);
+  return (rapQuotes[randomIndex]);
+}
+
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      randomQuote: {}
+    }
+
+    this.fetchQuote= this.fetchQuote.bind(this);
+  }
+
+  fetchQuote() {
+    console.log("fetching quotes...");
+    this.setState({
+      randomQuote: getRandomQuote()
+    });
+  }
+
+  componentDidMount() {
+    this.fetchQuote();
+  }
+
+  render() {
+    const {quote, author} = this.state.randomQuote;
+    return (
+          <div id="wrapper">
+      
+            <div id="quote-box">
+      
+              <div id="text">
+                <h2>{quote}</h2>
+              </div>
+      
+              <div id="author">
+                <p>-{author}</p>
+              </div>
+      
+              <div id="edit-quote">
+      
+                <div id="share-quote">
+      
+                  <a id="tweet-quote" 
+                  href={`https://twitter.com/intent/tweet?text=${encodeURIComponent(`"${this.state.text}" - ${this.state.author}`)}`} 
+                  target="_blank" rel="noopener noreferrer}"
+                >
+                    <i className="bi bi-twitter"></i>
+                  </a>
+      
+                </div>
+      
+                <button 
+                  id="new-quote" 
+                  type="button" 
+                  onClick={this.fetchQuote}
+                >
+                  New Quote
+                </button>
+      
+              </div>
+      
+            </div>
+      
+            <p id="project-credit">
+              By  <a id="project-author" href="https://github.com/Hilda-Enyioko" target="_blank" rel="noopener noreferrer">Hilda Enyioko</a>
+            </p>
+      
+          </div>
+    )
+  }
+
+}
+
+export default App;
