d6f4125adc19e315aecc83978c0690a0ccec89b7
Hilda Enyioko <enyiokohilda@gmail.com>
2025-01-19 18:20:25 +0100

    js functionalities completed

--- markdown-previewer/script.js
+++ markdown-previewer/script.js
@@ -7,4 +7,33 @@ function previewText() {
     preview.innerHTML = html;
 }
 
+function initializeText() {
+    const defaultMarkdown = `
+# Welcome to the Markdown Previewer
+---
+
+This is a simple markdown previewer created by: [**Hilda Enyioko**](https://github.com/Hilda-Enyioko).
+
+
+## Features
+---
+- Live Preview
+- Easy to Use
+
+### You can add:
+
+### A Heading
+\`Inline Code\`
+\`\`\`
+Block Quote
+\`\`\`
+
+![An Image](./image.jpg)
+`;
+
+    editor.value = defaultMarkdown;
+    previewText();
+}
+
 editor.addEventListener('input', previewText);
+initializeText();
