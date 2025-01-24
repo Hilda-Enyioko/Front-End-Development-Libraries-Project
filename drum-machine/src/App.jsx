94ea980f47c53ca05aa97d79f173dfbab40ad998
Hilda Enyioko <enyiokohilda@gmail.com>
2025-01-24 17:37:59 +0100

    all tests passed, improvements to be done

--- drum-machine/src/App.jsx
+++ drum-machine/src/App.jsx
@@ -1,9 +1,85 @@
 import React from 'react'
 
+const keysToDisplay = [
+  {key: 'Q', string: 'Heater 1'},
+  {key: 'W', string: 'Heater 2'},
+  {key: 'E', string: 'Heater 3'},
+  {key: 'A', string: 'Heater 4'},
+  {key: 'S', string: 'Heater 6'},
+  {key: 'D', string: 'Disc Oh'},
+  {key: 'X', string: 'Kick n Hat'},
+  {key: 'Z', string: 'Kick 1'},
+  {key: 'C', string: 'Cev H2'}
+]
 class App extends React.Component {
 
   constructor(props) {
     super(props);
+    this.state = {
+      display: '',
+      volume: 0.5,
+      togglePlay: true
+    }
+  }
+
+  handleKeyPress = (event) => {
+    const key = event.key.toUpperCase();
+    console.log(key);
+    const keyInfo = keysToDisplay.find(k => k.key === key);
+    console.log(keyInfo);
+
+    if(keyInfo) {
+      const audio = document.querySelector(`audio[id="${key}"]`);
+      if (audio) {
+        audio.currentTime = 0;
+        audio.volume = this.state.volume;
+        audio.play();
+  
+        this.setState({
+          display: keyInfo.string
+        })
+    }
+    }
+  }
+
+  handleClick = (key) => {
+    const keyInfo = keysToDisplay.find(k => k.key === key);
+    console.log(keyInfo);
+
+    if(keyInfo) {
+      const audio = document.querySelector(`audio[id="${key}"]`);
+      if (audio) {
+        audio.currentTime = 0;
+        audio.volume = this.state.volume;
+        audio.play();
+  
+        this.setState({
+          display: keyInfo.string
+        })
+    }
+    }
+  }
+
+  handleVolume = (event) => {
+    const volume = event.target.value / 100;
+    this.setState({volume});
+    console.log(this.state.volume);
+  }
+
+  // handlePlay = () => {
+  //   this.setState({
+  //     if (togglePlay) {togglePlay: false}
+  //     else {togglePlay: true}
+  //   }
+  //   )
+  // }
+
+  componentDidMount() {
+    document.addEventListener('keydown', this.handleKeyPress);
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener('keydown', this.handleKeyPress);
   }
 
   render() {
@@ -14,17 +90,17 @@ class App extends React.Component {
 
           <div className='key-row' id='key-row1'>
 
-            <div className='drum-pad' id='Q'>
+            <div className='drum-pad' id='Q' key={'Q'} onClick={() => this.handleClick('Q')}>
               <kbd>Q</kbd>
               <audio className='clip' id='Q' src='https://cdn.freecodecamp.org/testable-projects-fcc/audio/Heater-1.mp3' />
             </div>
 
-            <div className='drum-pad' id='W'>
+            <div className='drum-pad' id='W' key={'W'} onClick={() => this.handleClick('W')}>
               <kbd>W</kbd>
               <audio className='clip' id='W' src='https://cdn.freecodecamp.org/testable-projects-fcc/audio/Heater-2.mp3' />
             </div>
 
-            <div className='drum-pad' id='E'>
+            <div className='drum-pad' id='E' key={'E'} onClick={() => this.handleClick('E')}>
               <kbd>E</kbd>
               <audio className='clip' id='E' src='https://cdn.freecodecamp.org/testable-projects-fcc/audio/Heater-3.mp3' />
             </div>
@@ -33,17 +109,17 @@ class App extends React.Component {
 
           <div className='key-row' id='key-row2'>
 
-            <div className='drum-pad' id='A'>
+            <div className='drum-pad' id='A' key={'A'} onClick={() => this.handleClick('A')}>
               <kbd>A</kbd>
               <audio className='clip' id='A' src='https://cdn.freecodecamp.org/testable-projects-fcc/audio/Heater-4_1.mp3' />
             </div>
 
-            <div className='drum-pad' id='S'>
+            <div className='drum-pad' id='S' key={'S'} onClick={() => this.handleClick('S')}>
               <kbd>S</kbd>
               <audio className='clip' id='S' src='https://cdn.freecodecamp.org/testable-projects-fcc/audio/Heater-6.mp3' />
             </div>
 
-            <div className='drum-pad' id='D'>
+            <div className='drum-pad' id='D' key={'D'} onClick={() => this.handleClick('D')}>
               <kbd>D</kbd>
               <audio className='clip' id='D' src='https://cdn.freecodecamp.org/testable-projects-fcc/audio/Dsc_Oh.mp3' />
             </div>
@@ -52,17 +128,17 @@ class App extends React.Component {
 
           <div className='key-row' id='key-row3'>
 
-            <div className='drum-pad' id='Z'>
+            <div className='drum-pad' id='Z' key={'Z'} onClick={() => this.handleClick('Z')}>
               <kbd>Z</kbd>
               <audio className='clip' id='Z' src='https://cdn.freecodecamp.org/testable-projects-fcc/audio/Kick_n_Hat.mp3' />
             </div>
 
-            <div className='drum-pad' id='X'>
+            <div className='drum-pad' id='X' key={'X'} onClick={() => this.handleClick('X')}>
               <kbd>X</kbd>
               <audio className='clip' id='X' src='https://cdn.freecodecamp.org/testable-projects-fcc/audio/RP4_KICK_1.mp3' />
             </div>
 
-            <div className='drum-pad' id='C'>
+            <div className='drum-pad' id='C' key={'C'} onClick={() => this.handleClick('C')}>
               <kbd>C</kbd>
               <audio className='clip' id='C' src='https://cdn.freecodecamp.org/testable-projects-fcc/audio/Cev_H2.mp3' />
             </div>
@@ -83,9 +159,19 @@ class App extends React.Component {
             </div>
           </div>
 
-          <div id='display'></div>
+          <div id='display'>
+            <p>{this.state.display}</p>
+          </div>
 
-          <input type='range' name='volume' id='volume-control' min='0' max='100' step='5' />
+          <input 
+          type='range' 
+          name='volume' 
+          id='volume-control' 
+          min='0' 
+          max='100' 
+          step='5'
+          onChange = {this.handleVolume}
+          />
 
         </div>
 
